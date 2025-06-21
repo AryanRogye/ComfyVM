0aba7238e7fe683c7a1e5b2ceba81630de0f7af7
AryanRogye <aryan.rogye@gmail.com>
2025-06-21 15:57:45 -0500

    first commit

---
+++ ComfyVM/ComfyVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComfyVMApp.swift
+//  ComfyVM
+//
+//  Created by Aryan Rogye on 6/21/25.
+//
+
+import SwiftUI
+
+@main
+struct ComfyVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
